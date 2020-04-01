e5dbfa97a92d61ac012b20bb5dfd3e380d207d01
zas <silvan.zahno@hevs.ch>
2020-04-01 16:22:02 +0200

    ADD: dashboard hevslib submodule ADD: v0.1 of Dashboard script ADD: all current dashboard input data ADD: projectlist.xls

--- 04_HEVS/Dashboard/base_config.py
+++ 04_HEVS/Dashboard/base_config.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python3
-# Dashboard Config
-
 ###############################################################################
-# Confidentiality
-# **All information in this document is strictly confidiental**
-# **Copyright (C) 2020 HES-SO Valais-Wallis - All Rights Reserved**
-
+#          __ _____            __
+#   /\  /\/__\\_   \   /\   /\/ _\
+#  / /_/ /_\   / /\/___\ \ / /\ \
+# / __  //__/\/ /_|_____\ V / _\ \
+# \/ /_/\__/\____/       \_/  \__/
+# (c) - zas
+###############################################################################
+# Dashboard - base config
+# **Copyright (C) 2019 HES-SO Valais-Wallis - All Rights Reserved**
+###############################################################################
 import enum
 import pandas as pd
+
+from hevslib.plotly import *
 
 ###############################################################################
 # Constants
 #
-# Month - Day
-month2year = 1/12
-year2month = 12
-day2month = 0.03285421
-month2day = 1/day2month
+# 0 = no output
+# 1 = normal output
+# 2 = verbose output
+verbose = 2
+
+
 
 # Data Storage constants
 data_inputDir = "in"
@@ -24,11 +31,19 @@ data_outputDir = "out"
 
 # Project Definition
 projectListFile = "projectList.xls"
-projectListColumns = ["project_number", "acronym", "title_humanreadable", "budget_total", "budget_material", "date_begin", "date_end"]
+projectCol = {"project_number": "project_number",
+              "acronym": "acronym",
+              "title_humanreadable": "title_humanreadable",
+              "budget_total": "budget_total",
+              "budget_material": "budget_material",
+              "date_begin": "date_begin",
+              "date_end": "date_end",
+              "updated": "updated",
+              }
 
 # activity constans
 activityData = []
-activityDfColumns = ["german", "french"]
+activityCol = ["german", "french"]
 activityData = [["Allgemeine administrative Arbeiten", "Tâches administratives générales"],
                 ["Durchführung von aF&E Projekten oder -Arbeiten", "Réalisation de projects ou d'activités Ra&D"],
                 ["Durchführung von Mandaten", "Réalisation de mandats"],
@@ -36,50 +51,39 @@ activityData = [["Allgemeine administrative Arbeiten", "Tâches administratives 
                 ["Unterricht (Anstellungsplan)", "Cours dispensé (plan d'engagement)"],
                 ["Unterrichtsspezifische Aufgaben (1.4)", "Tâche spécifiques d'enseignement"],
                 ["Weiterbildung", "Formation"],
-               ]
-activityDf = pd.DataFrame(activityData, columns=activityDfColumns)
+                ]
+activityDf = pd.DataFrame(activityData, columns=activityCol)
 
 # Input ash file config
-ash_sheetname = "Sheet1"
+ashSheetname = "Sheet1"
 
-ash_columns = { 'german' : ["Datum",
-                  "Mitarbeiter",
-                  "Tätigkeit",
-                  "Zugeteilter Satz",
-                  "Anz. Stunden",
-                  "Betrag",
-                  "Beschrieb",                  
-                 ],
-               'french' : ["Date",
-                  "Collaborateur",
-                  "Activité",
-                  "Taux affecté",
-                  "Nb heures",
-                  "Montant",
-                  "Description"
-                  ]
-              }
+ashCols = {'german': {"date": "Datum",
+                      "collaborator": "Mitarbeiter",
+                      "activity": "Tätigkeit",
+                      "rate": "Zugeteilter Satz",
+                      "hours": "Anz. Stunden",
+                      "amount": "Betrag",
+                      "description": "Beschrieb",
+                     },
+          'french': {"date": "Date",
+                     "collaborator": "Collaborateur",
+                     "activity": "Activité",
+                     "rate": "Taux affecté",
+                     "hours": "Nb heures",
+                     "amount": "Montant",
+                     "description": "Description"
+                    }
+          }
+
 
 ###############################################################################
 # Graph output Options
 #
-class GraphOutputOption(enum.Enum):
-    none = 'none'                # Do not generate any plots
-    inline = 'inline'            # Generate inline plots only
-    htmlFile = 'extFile'         # Generate plots in external file (html|png|...)
-    both = 'both'                # Generate all plots inline and external
-
-
-class GraphInteractionOption(enum.Enum):
-    static = 'static'            # Generate static inline plots (as images)
-    interactive = 'interactive'  # Generate interactive inline plots
-
-
 notebookGraphicInteraction = GraphInteractionOption('static')
 notebookGraphicOutputs = GraphOutputOption('both')
 
 ext_file = ".svg"
 
-staticImageSize = {'width':1280, 'height':720, 'scale':1}
+staticImageSize = {'width': 1280, 'height': 720, 'scale': 1}
 
-GraphAutoOpenHTML = False        # Auto open external HTML files [True/False]
+GraphAutoOpenHTML = False  # Auto open external HTML files [True/False]
